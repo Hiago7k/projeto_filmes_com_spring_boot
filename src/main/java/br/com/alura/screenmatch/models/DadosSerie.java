8ac05cb2e450fcaeefc50f2ecdd00ae8a1caac81
Hiago 7k <hiago7k@gmail.com>
2026-08-10 20:30:20 -0300

    utilizando alias

--- src/main/java/br/com/alura/screenmatch/models/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/models/DadosSerie.java
@@ -1,7 +1,9 @@
 package br.com.alura.screenmatch.models;
 
-public record DadosSerie(String titulo,
-                         Integer totalTemporadas,
-                         String avaliacao
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao
                          ) {
 }
