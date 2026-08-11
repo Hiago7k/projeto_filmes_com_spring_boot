e42d89d04c1370da386d62a00e684d3ab036209a
Hiago 7k <hiago7k@gmail.com>
2026-08-11 16:58:38 -0300

    type genercis

--- src/main/java/br/com/alura/screenmatch/models/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/models/DadosSerie.java
@@ -4,6 +4,5 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 
 public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
-                         @JsonAlias("imdbRating") String avaliacao
-                         ) {
+                         @JsonAlias("imdbRating") String avaliacao) {
 }
---
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -0,0 +1,11 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.models.DadosSerie;
+import tools.jackson.databind.ObjectMapper;
+
+public class ConverteDados {
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+
+}
