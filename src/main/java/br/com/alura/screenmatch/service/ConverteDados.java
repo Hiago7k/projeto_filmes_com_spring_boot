03d84e6e6a62ba41d61f8358fd8530a66a5c3cd6
Hiago 7k <hiago7k@gmail.com>
2026-08-11 19:02:37 -0300

    episodios

--- src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -6,7 +6,6 @@ import tools.jackson.databind.ObjectMapper;
 public class ConverteDados implements IConverteDados {
     private ObjectMapper mapper = new ObjectMapper();
 
-
     @Override
     public <T> T obterDados(String json, Class<T> classe) {
         return mapper.readValue(json, classe);
