73c1728916a525e2844051b9817ee98160c9dc9a
Hiago 7k <hiago7k@gmail.com>
2026-08-13 16:58:11 -0300

    salvando as temporadas

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.models.DadosEpisodio;
 import br.com.alura.screenmatch.models.DadosSerie;
+import br.com.alura.screenmatch.models.DadosTemporadas;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -39,5 +43,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		DadosEpisodio dadosEp = conversor.obterDados(jsonEp, DadosEpisodio.class);
 		System.out.println(dadosEp);
 
+        List<DadosTemporadas>  temporadas = new ArrayList<>();
+
+		for (int i = 1 ; i<= dados.totalTemporadas(); i++){
+			var jsonTotalTmp = consumoApiEp.obterDados("https://www.omdbapi.com/?t=the+walking+dead&season=" + i + "apikey=6f533f65");
+			DadosTemporadas dadosTemporadas = conversor.obterDados(jsonTotalTmp, DadosTemporadas.class);
+			temporadas.add(dadosTemporadas);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
