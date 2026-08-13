acd85b37fd145078b1e516ee76f86e9ca457a57c
Hiago 7k <hiago7k@gmail.com>
2026-08-13 16:49:36 -0300

    modelando temporadas

---
+++ src/main/java/br/com/alura/screenmatch/models/DadosTemporadas.java
@@ -0,0 +1,10 @@
+package br.com.alura.screenmatch.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+import java.util.List;
+
+public record DadosTemporadas(@JsonAlias("Season") Integer numero,
+                              @JsonAlias("Episodes") List<DadosEpisodio> episodios
+                              ) {
+}
