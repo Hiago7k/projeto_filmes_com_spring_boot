2d18064446357663915014fb973430dd2838b3ad
Hiago 7k <hiago7k@gmail.com>
2026-08-17 19:34:22 -0300

    puxando conversor

--- src/main/java/br/com/alura/screenmatch/Principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/Principal/Principal.java
@@ -1,12 +1,15 @@
 package br.com.alura.screenmatch.Principal;
 
+import br.com.alura.screenmatch.models.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.util.Scanner;
 
 public class Principal {
 private Scanner leitura = new Scanner(System.in);
 private ConsumoApi consumo = new ConsumoApi();
+private ConverteDados conversor = new ConverteDados();
 private final String ENDERECO = "https://www.omdbapi.com/?t=";
 private final String API_KEY = "&apikey=6f533f65";
 
@@ -14,7 +17,6 @@ private final String API_KEY = "&apikey=6f533f65";
         System.out.println("Digite o nome da série para busca");
         var nomeSerie = leitura.next();
         var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
-        //the+walking+dead
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
     };
-
 }
