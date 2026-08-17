c3f43ef6b0ececb0fbcb88442d15b3faf936560b
Hiago 7k <hiago7k@gmail.com>
2026-08-17 19:23:24 -0300

    criando var constante

--- src/main/java/br/com/alura/screenmatch/Principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/Principal/Principal.java
@@ -1,9 +1,16 @@
 package br.com.alura.screenmatch.Principal;
 
+import java.util.Scanner;
+
 public class Principal {
+private Scanner leitura = new Scanner(System.in);
+private final String ENDERECO = "https://www.omdbapi.com/?t=";
+private final String API_KEY = "&apikey=6f533f65";
 
     public void exibeMenu(){
-
+        System.out.println("Digite o nome da série para busca");
+        var nomeSerie = leitura.next();
+        //the+walking+dead
     };
 
 }
