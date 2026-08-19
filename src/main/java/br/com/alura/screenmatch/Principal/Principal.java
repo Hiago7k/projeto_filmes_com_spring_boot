fce9caf15f010efaf7c80b53728d682b54d40742
Hiago 7k <hiago7k@gmail.com>
2026-08-19 18:55:06 -0300

    refatorando

--- src/main/java/br/com/alura/screenmatch/Principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/Principal/Principal.java
@@ -34,11 +34,8 @@ private final String API_KEY = "&apikey=6f533f65";
 		}
 		temporadas.forEach(System.out::println);
 
-        for (int i = 0; i < dados.totalTemporadas(); i++){
-            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-            for(int j = 0; j < episodiosTemporada.size(); j++){
-                System.out.println(episodiosTemporada.get(j).titulo());
-            }
-        }
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+
     };
 }
